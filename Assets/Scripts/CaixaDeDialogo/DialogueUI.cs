b7ffb037fcd090c915ea27b59c723c9148ce18c9
Kelvin Santos <kelvin.didiei3@gmail.com>
2021-12-09 15:23:00 -0300

    Retrato na Caixa de Dialogo
    Agora os os retratos das caixas de diálogo são exibidos e a área do texto se ajusta conforme tem um retrato para exibir ou não.

--- Assets/Scripts/CaixaDeDialogo/DialogueUI.cs
+++ Assets/Scripts/CaixaDeDialogo/DialogueUI.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DialogueUI : MonoBehaviour
 {
     //Componentes
     [SerializeField] private GameObject dialogueBox; //Guarda toda a caixa de dialogo
     [SerializeField] private TMP_Text textLabel; //Guarda a caixa de texto
+    [SerializeField] private Image portrait; //Guarda o retrato da caixa de dialogo 
     private DialogueActivator dialogueActivator;
 
     private ResponseEvent dialogueEndEvents;
@@ -17,6 +19,8 @@ public class DialogueUI : MonoBehaviour
     private ResponseHandler responseHandler;
     private TypewriterEffect typewriterEffect;
 
+    private float textLabelLeftBorder;
+
     void Start()
     {
         //Componentes
@@ -26,6 +30,23 @@ public class DialogueUI : MonoBehaviour
         player = FindObjectOfType<Player>();
 
         CloseDialogueBox();
+    }
+
+    //Atualiza a imagem do retrato e a borda esquerda da caixa de texto
+    private void UpdateImage(DialogueObject.DialogueStruct dialogue)
+    {
+        if(dialogue.portrait != null)
+        {
+            textLabel.margin = new Vector4(dialogue.portrait.rect.width, 0, 0, 0);
+            Debug.Log("Tamanho da imagem: " + dialogue.portrait.rect.width);
+            portrait.gameObject.SetActive(true);
+            portrait.sprite = dialogue.portrait;
+        }
+        else
+        {
+            textLabel.margin = new Vector4(0, 0, 0, 0);
+            portrait.gameObject.SetActive(false);
+        }
     }
 
     public void UpdateDialogueActivator(DialogueActivator dialogueActivator)
@@ -60,6 +81,7 @@ public class DialogueUI : MonoBehaviour
     {
         for(int i = 0; i < dialogueObject.Dialogue.Length; i++)
         {
+            UpdateImage(dialogueObject.Dialogue[i]);
             string dialogue = dialogueObject.Dialogue[i].text;
 
             yield return RunTypingEffect(dialogue);
@@ -103,6 +125,7 @@ public class DialogueUI : MonoBehaviour
     public void CloseDialogueBox()
     {
         IsOpen = false;
+        portrait.gameObject.SetActive(false);
         dialogueBox.SetActive(false);
         textLabel.text = string.Empty;
     }
