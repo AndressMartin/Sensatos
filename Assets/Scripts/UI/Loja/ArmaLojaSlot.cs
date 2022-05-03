a9b0e8306a3b3355676ad1374a3c58e026373eb6
Kelvin Santos <kelvin.didiei3@gmail.com>
2022-05-03 16:25:22 -0300

    Loja de Armas e Inventário de Loja
    A loja de armas começou a ser implementado.
    O scriptable object de inventário de loja foi adicionado no jogo.

--- Assets/Scripts/UI/Loja/ArmaLojaSlot.cs
+++ Assets/Scripts/UI/Loja/ArmaLojaSlot.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArmaLojaSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Componentes
+    [SerializeField] private TMP_Text nome;
+    [SerializeField] private TMP_Text preco;
+    [SerializeField] private Image imagem;
+    [SerializeField] private Image imagemMunicao;
+
+    private Image imagemFundo;
+
+    //Variaveis
+    private bool iniciado = false;
+
+    private void Start()
     {
-        
+        Iniciar();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Iniciar()
     {
-        
+        if (iniciado == true)
+        {
+            return;
+        }
+
+        imagemFundo = GetComponent<Image>();
+
+        iniciado = true;
+    }
+
+    public void ZerarInformacoes()
+    {
+        imagem.sprite = null;
+        imagemMunicao.gameObject.SetActive(false);
+        nome.text = "";
+        preco.text = "";
+    }
+
+    public void AtualizarInformacoes(ArmaDeFogo arma, int preco)
+    {
+        imagem.sprite = arma.ImagemInventario;
+        nome.text = arma.Nome;
+        this.preco.text = preco.ToString();
+    }
+
+    public void ImagemMunicaoAtiva(bool ativa)
+    {
+        imagemMunicao.gameObject.SetActive(ativa);
+    }
+
+    public void Selecionado(bool selecionado)
+    {
+        if (selecionado == true)
+        {
+            imagemFundo.color = Color.blue;
+        }
+        else
+        {
+            imagemFundo.color = Color.red;
+        }
     }
 }
